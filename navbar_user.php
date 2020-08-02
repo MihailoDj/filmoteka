a59ac06c65907c0023d6133adc4be6f9f444e4bc
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2020-08-02 22:09:49 +0200

    Zapoceta implementacija podesavanja naloga

--- navbar_user.php
+++ navbar_user.php
@@ -1,12 +1,12 @@
 <div class="navbar">
     <i class="fas fa-bars fa-2x menu"></i>
     <a href="login.php"><i class="fas fa-sign-out-alt"></i> Izloguj se</a>
-    <a href="settings.php"><i class="fas fa-cog"></i> Podesavanja naloga</a>
+    <a href="user_settings.php"><i class="fas fa-cog"></i> Podesavanja naloga</a>
 </div>
 <div class="side-nav">
     <button class="btn-close" style="color: yellow;">&times;</button>
     <br><br>
-    <a href="settings.php"><i class="fas fa-cog"></i> Podesavanja</a>
+    <a href="user_settings.php"><i class="fas fa-cog"></i> Podesavanja</a>
     <a href="login.php"><i class="fas fa-sign-out-alt"></i> Izloguj se</a>
 </div>
 <script src="js/menu.js?<?php echo date('l jS \of F Y h:i:s A'); ?>"></script>
