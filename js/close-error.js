16100cc717a4a757eda3e6613294e8ee84739e43
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2020-08-16 20:47:58 +0200

    Ispravljeno prevremeno nestajanje alertova

--- js/close-error.js
+++ js/close-error.js
@@ -2,13 +2,17 @@ $(document).on('click','.close-error',function(){
     $(".rezultat").fadeOut();
 })
 
-function checkIfVisible() {
-    setInterval(function () {
+$(document).on('click', '.btn', function() {
+    setTimeout(() => {
+        $(".rezultat").fadeOut();
+    }, 5000);
+})
+
+window.addEventListener("keypress", e => {
+    var key = e.which || e.keyCode;
+    if (key === 13) {
         setTimeout(() => {
             $(".rezultat").fadeOut();
         }, 5000);
-        checkIfVisible();
-    }, 1000);
-}
-
-checkIfVisible();
+    }
+});
